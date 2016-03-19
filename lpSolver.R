8f9710f38a91f1dbec57a706d9b3eb32a64fff7f
Tom Shott <tshott@cecs.pdx.edu>
2016-03-18 17:06:46 -0700

    Started lpSolver S4 API

---
+++ lpSolver.R
@@ -0,0 +1,62 @@
+#
+# Test Solver Class
+#
+
+require(methods)
+
+#
+#
+# lpSolver Class
+#
+#
+
+setClass("lpSolver",
+         slots = c(
+           modelname = "character",
+           constraints = "matrix",
+           rhs = "numeric",
+           obj = "numeric",
+           sense = "character",                       # Linear constraint type - >=, ==,
+           lb = "numeric",
+           ub = "numeric",
+           modelsense = "character"
+         )
+)
+
+
+#
+# Carefull - how chane size without creating error on the fly?
+
+validlpSoverObject <- function(object){
+
+    # Check Character values with limited values
+    if (length(object@modelsense) > 0 &&
+        object@modelsense %in% c("min", "max")){
+      return("Not min or max")
+    }
+
+    return(TRUE)
+}
+
+#setValidity("lpSolver", validlpSoverObject)
+
+lpq_good <- new("lpSolver",
+                modelname="DEA CCR",
+                constraints = matrix( c(1, 2, 3, 4), nrow=2, byrow=TRUE),
+                rhs = c(5, 6, 7, 8)
+)
+
+# lpq_bad1 <- new("lpSolver",
+#                 name=3,
+#                 constraints = c(1, 2, 3, 4),
+#                 rhs = c(5, 6, 7, 8)
+# )
+
+
+model <- new("lpSolver")
+model@constraints   <- matrix(c(1,1,0,0,1,1), nrow=2, byrow=T)
+model@obj           <- c(1,1,2)
+model@modelsense    <- "max"
+model@rhs           <- c(1,1)
+model@sense         <- c("<=", "<=")
+
