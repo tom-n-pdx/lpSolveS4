831cdb37d54615bc13cf4103f772e7f98ede0bc4
Tom Shott <tshott@cecs.pdx.edu>
2016-03-18 17:55:10 -0700

    Updated to check values

--- lpSolver.R
+++ lpSolver.R
@@ -26,14 +26,25 @@ setClass("lpSolver",
 
 #
 # Carefull - how chane size without creating error on the fly?
-
+#
+# ToDo - Make check all vales?
 validlpSoverObject <- function(object){
 
-    # Check Character values with limited values
-    if (length(object@modelsense) > 0 &&
-        object@modelsense %in% c("min", "max")){
-      return("Not min or max")
+  # Check Character values with limited values
+  if (length(object@modelsense) > 0 &&
+      ! object@modelsense %in% c("min", "max")){
+    return("modelsense Not min or max")
+  }
+
+  sense_legal.l <- c("<=", "==", ">=", "")
+  if (length(object@sense) > 0){
+    for (i in 1:length(object@sense)){
+      if (! object@sense[i] %in% sense_legal.l){
+        return("sense contains illegal value")
+      }
     }
+  }
+
 
     return(TRUE)
 }
@@ -53,10 +64,10 @@ lpq_good <- new("lpSolver",
 # )
 
 
-model <- new("lpSolver")
-model@constraints   <- matrix(c(1,1,0,0,1,1), nrow=2, byrow=T)
-model@obj           <- c(1,1,2)
-model@modelsense    <- "max"
-model@rhs           <- c(1,1)
-model@sense         <- c("<=", "<=")
+lpq_bad <- new("lpSolver")
+lpq_bad@constraints   <- matrix(c(1,1,0,0,1,1), nrow=2, byrow=T)
+lpq_bad@obj           <- c(1,1,2)
+lpq_bad@modelsense    <- "max"
+lpq_bad@rhs           <- c(1,1)
+lpq_bad@sense         <- c("<=", "<=")
 
