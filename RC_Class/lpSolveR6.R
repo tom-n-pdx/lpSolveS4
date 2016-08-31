e990674e0f0c93ac9c2d8a3e1b4cfccfdc17814d
Tom Shott <tshott@cecs.pdx.edu>
2016-08-31 15:48:47 -0700

    Updated lpSolveR6 print

--- RC_Class/lpSolveR6.R
+++ RC_Class/lpSolveR6.R
@@ -13,6 +13,76 @@ sense_legal.l <- c("free", "<=", ">=", "=")
 type_legal.l  <- c("real", "integer", "binary")
 
 
+lpShow = function(digits=NA) {
+
+  cat("lpSolve show: ", self$modelname, "\n")
+
+  # Get Size
+  if (length(self$A) < 2){
+    cat("Can't show lpSolve object with undefined constraints - falling back to debug print\n")
+    self$print()
+    return()
+  }
+
+  # Get digits option from environemnt
+  digits <- ifelse(!is.na(digits), digits, getOption("digits"))
+
+  width  <- digits + 3
+  format_b <- "%5.5s"
+  format_s <- paste0(" %", width, ".", width,  "s", collapse = "")
+  format_g <- paste0(" %", width, ".", digits, "g", collapse = "")
+
+
+  col.n     <- ncol(self$A)
+  col.names <- colnames(self$A, do.NULL=FALSE, prefix = "C")
+
+  row.n     <- nrow(self$A)
+  row.names <- rownames(self$A, do.NULL=FALSE, prefix = "R")
+
+  # temp_cons <- self$A
+  # Build the format strings we'll need - one for lines w/ strings, one for lines numbers
+  row_fmt_s  <- paste0(c(format_b, rep_len(format_s, col.n)),
+                       collapse="")
+  row_fmt_g  <- paste0(c(format_b, rep_len(format_g, col.n), " %4s", format_g),
+                       collapse="")
+
+
+  # Col Names
+  cat("    ", paste0(sprintf(format_s, col.names), collapse=""), "\n")
+  # cat("format: ", row_fmt_s, "\n")
+  # cat(sprintf(row_fmt_s, col.names, col.names), "\n")
+  cat(sprintf(row_fmt_s, c("XX", col.names)), "\n")
+
+  # Min/Max & Objective
+  sense_str <- sprintf("%5.5s", self$modelsense)
+  obj_str   <- paste0(sprintf(format_g, rep_len(self$obj, col.n)), collapse="")
+  cat(paste0(sense_str, obj_str, collapse=""), "\n")
+
+  # row name, constraint row, sense, rhs
+  for (i in 1:row.n){
+    name_str  <- sprintf("%5.5s", row.names[i])
+    cons_str  <- paste0(sprintf(format_g, rep_len(self$A[i,], col.n)), collapse="")
+    sense_str <- sprintf(" %4s", rep_len(self$sense, row.n)[i])
+    rhs_str   <- sprintf(format_g, rep_len(self$rhs,   row.n)[i])
+    cat(paste0(name_str, cons_str, sense_str, rhs_str, collapse=""), "\n")
+  }
+
+  # upper & lower bounds
+  ub_str <- paste0( sprintf(format_g, rep_len(self$ub, col.n)), collapse="")
+  cat(paste0("Upper", ub_str, collapse=""), "\n")
+
+  lb_str <- paste0( sprintf(format_g, rep_len(self$lb, col.n)), collapse="")
+  cat(paste0("Lower", lb_str, collapse=""), "\n")
+
+  # type
+  type_str <- paste0( sprintf(format_s, rep_len(self$type, col.n)), collapse="")
+  cat(paste0("Type ", type_str, collapse=""), "\n")
+
+  invisible(self)
+}
+
+
+
 lpSolveR6 <- R6Class("lpSolveR6",
   public = list(
 
@@ -53,7 +123,7 @@ lpSolveR6 <- R6Class("lpSolveR6",
 
 
     print = function() {
-      if (!is.na(self$A)){
+      if (length(self$A) >= 2){
         col.n     <- ncol(self$A)
         row.n     <- nrow(self$A)
       } else {
@@ -66,71 +136,73 @@ lpSolveR6 <- R6Class("lpSolveR6",
       invisible(self)
     },
 
-    show = function(digits=NA) {
+    show = lpShow
 
-      cat("lpSolve show: ", self$modelname, "\n")
-
-      # Get digits option from environemnt
-      digits <- ifelse(!is.na(digits), digits, getOption("digits"))
-
-      width  <- digits + 3
-      format_b <- "%5.5s"
-      format_s <- paste0(" %", width, ".", width,  "s", collapse = "")
-      format_g <- paste0(" %", width, ".", digits, "g", collapse = "")
-
-      # Get Size
-      if (length(self$A) < 2){
-        cat("Can't show lpSolve object with undefined constraints - falling back to debug print\n")
-        self$print()
-        return()
-      }
-
-      col.n     <- ncol(self$A)
-      col.names <- colnames(self$A, do.NULL=FALSE, prefix = "C")
-
-      row.n     <- nrow(self$A)
-      row.names <- rownames(self$A, do.NULL=FALSE, prefix = "R")
-
-      # temp_cons <- self$A
-      # Build the format strings we'll need - one for lines w/ strings, one for lines numbers
-      # row_fmt_s  <- paste0(c(format_b, rep_len(format_s, col.n)),
-      #                      collapse="")
-      # row_fmt_g  <- paste0(c(format_b, rep_len(format_g, col.n), " %4s", format_g),
-      #                      collapse="")
-
-
-      # Col Names
-      cat("    ", paste0(sprintf(format_s, col.names), collapse=""), "\n")
-      # cat("format: ", row_fmt_s, "\n")
-      # cat(sprintf(row_fmt_s, col.names, col.names), "\n")
-
-      # Min/Max & Objective
-      sense_str <- sprintf("%5.5s", self$modelsense)
-      obj_str   <- paste0(sprintf(format_g, rep_len(self$obj, col.n)), collapse="")
-      cat(paste0(sense_str, obj_str, collapse=""), "\n")
-
-      # row name, constraint row, sense, rhs
-      for (i in 1:row.n){
-        name_str  <- sprintf("%5.5s", row.names[i])
-        cons_str  <- paste0(sprintf(format_g, rep_len(self$A[i,], col.n)), collapse="")
-        sense_str <- sprintf(" %4s", rep_len(self$sense, row.n)[i])
-        rhs_str   <- sprintf(format_g, rep_len(self$rhs,   row.n)[i])
-        cat(paste0(name_str, cons_str, sense_str, rhs_str, collapse=""), "\n")
-      }
-
-      # upper & lower bounds
-      ub_str <- paste0( sprintf(format_g, rep_len(self$ub, col.n)), collapse="")
-      cat(paste0("Upper", ub_str, collapse=""), "\n")
-
-      lb_str <- paste0( sprintf(format_g, rep_len(self$lb, col.n)), collapse="")
-      cat(paste0("Lower", lb_str, collapse=""), "\n")
-
-      # type
-      type_str <- paste0( sprintf(format_s, rep_len(self$type, col.n)), collapse="")
-      cat(paste0("Type ", type_str, collapse=""), "\n")
-
-      invisible(self)
-    }
+    # show = function(digits=NA) {
+    #
+    #   cat("lpSolve show: ", self$modelname, "\n")
+    #
+    #   # Get digits option from environemnt
+    #   digits <- ifelse(!is.na(digits), digits, getOption("digits"))
+    #
+    #   width  <- digits + 3
+    #   format_b <- "%5.5s"
+    #   format_s <- paste0(" %", width, ".", width,  "s", collapse = "")
+    #   format_g <- paste0(" %", width, ".", digits, "g", collapse = "")
+    #
+    #   # Get Size
+    #   if (length(self$A) < 2){
+    #     cat("Can't show lpSolve object with undefined constraints - falling back to debug print\n")
+    #     self$print()
+    #     return()
+    #   }
+    #
+    #   col.n     <- ncol(self$A)
+    #   col.names <- colnames(self$A, do.NULL=FALSE, prefix = "C")
+    #
+    #   row.n     <- nrow(self$A)
+    #   row.names <- rownames(self$A, do.NULL=FALSE, prefix = "R")
+    #
+    #   # temp_cons <- self$A
+    #   # Build the format strings we'll need - one for lines w/ strings, one for lines numbers
+    #   # row_fmt_s  <- paste0(c(format_b, rep_len(format_s, col.n)),
+    #   #                      collapse="")
+    #   # row_fmt_g  <- paste0(c(format_b, rep_len(format_g, col.n), " %4s", format_g),
+    #   #                      collapse="")
+    #
+    #
+    #   # Col Names
+    #   cat("    ", paste0(sprintf(format_s, col.names), collapse=""), "\n")
+    #   # cat("format: ", row_fmt_s, "\n")
+    #   # cat(sprintf(row_fmt_s, col.names, col.names), "\n")
+    #
+    #   # Min/Max & Objective
+    #   sense_str <- sprintf("%5.5s", self$modelsense)
+    #   obj_str   <- paste0(sprintf(format_g, rep_len(self$obj, col.n)), collapse="")
+    #   cat(paste0(sense_str, obj_str, collapse=""), "\n")
+    #
+    #   # row name, constraint row, sense, rhs
+    #   for (i in 1:row.n){
+    #     name_str  <- sprintf("%5.5s", row.names[i])
+    #     cons_str  <- paste0(sprintf(format_g, rep_len(self$A[i,], col.n)), collapse="")
+    #     sense_str <- sprintf(" %4s", rep_len(self$sense, row.n)[i])
+    #     rhs_str   <- sprintf(format_g, rep_len(self$rhs,   row.n)[i])
+    #     cat(paste0(name_str, cons_str, sense_str, rhs_str, collapse=""), "\n")
+    #   }
+    #
+    #   # upper & lower bounds
+    #   ub_str <- paste0( sprintf(format_g, rep_len(self$ub, col.n)), collapse="")
+    #   cat(paste0("Upper", ub_str, collapse=""), "\n")
+    #
+    #   lb_str <- paste0( sprintf(format_g, rep_len(self$lb, col.n)), collapse="")
+    #   cat(paste0("Lower", lb_str, collapse=""), "\n")
+    #
+    #   # type
+    #   type_str <- paste0( sprintf(format_s, rep_len(self$type, col.n)), collapse="")
+    #   cat(paste0("Type ", type_str, collapse=""), "\n")
+    #
+    #   invisible(self)
+    # }
 
   )
 )
